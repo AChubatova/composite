f4cfa593e8db5eb0d4adb1185a46b283a478344a
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-10-14 10:46:34 +0200

    Update py1.py

--- py1.py
+++ py1.py
@@ -1,3 +1,32 @@
-import time
-print("something")
-time.sleep(49) 
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers["Content-Type"] = "text/html"
+        self.response.write("""
+          <html>
+            <head><title>Enter your name...</title></head>
+            <body>
+              <form action="/welcome" method="post">
+                <input type="text" name="my_name"><br>
+                <input type="submit" value="Sign In">
+              </form>
+            </body>
+            </html>""")
+
+
+class Greeting(webapp2.RequestHandler):
+    def post(self):
+        username = self.request.get("my_name")
+        welcome_string = """<html><body>
+                          grüß Gott, {}!
+                          </body></html>""".format(username)
+        self.response.headers["Content-Type"] = "text/html"
+        self.response.write(welcome_string)
+
+
+routes = [('/', MainPage), ('/welcome', Greeting)]
+print("create webapp")
+print('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+my_app = webapp2.WSGIApplication(routes, debug=True)
