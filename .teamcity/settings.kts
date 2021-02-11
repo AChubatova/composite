5c28336e44d5a06c28fda1d7cfe8b129d074a80b
admin <admin@TeamCity>
2021-02-11 11:04:57 +0100

    TeamCity change in 'asp net' project: runners of 'asp net' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,7 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.DotnetBuildStep
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.dotnetBuild
-import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
@@ -50,9 +49,6 @@ object AspNet : BuildType({
             logging = DotnetBuildStep.Verbosity.Minimal
             sdk = "4"
             param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
-        }
-        script {
-            scriptContent = ""
         }
     }
 })
