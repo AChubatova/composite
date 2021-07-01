8778fa340bd8a9d4dc885039605e39a6de25a3f1
admin <admin@TeamCity>
2021-07-01 16:57:57 +0200

    TeamCity change in 'vcsproblem' project: runners of 'b' build configuration were updated

--- .teamcity/patches/buildTypes/B.kts
+++ .teamcity/patches/buildTypes/B.kts
@@ -1,6 +1,8 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.ScriptBuildStep
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
 
 /*
@@ -11,6 +13,18 @@ accordingly, and delete the patch script.
 changeBuildType(RelativeId("B")) {
     vcs {
         add(DslContext.settingsRoot.id!!)
+    }
+
+    expectSteps {
+        script {
+            scriptContent = "sleep 60"
+        }
+    }
+    steps {
+        update<ScriptBuildStep>(0) {
+            clearConditions()
+            scriptContent = "sleep 80"
+        }
     }
 
     requirements {
