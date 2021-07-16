bc21b0daa5c542714c3f069ee5fa48acb64bd39b
admin <admin@TeamCity>
2021-07-16 14:17:22 +0200

    TeamCity change in 'chkpr' project: triggers of 'chkprB' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -97,7 +97,7 @@ object ChkprB : BuildType({
             buildType = "${ChkprA.id}"
 
             buildParams {
-                password("pwpr", "credentialsJSON:0f844515-5548-4c90-90e3-5b540a02ae56")
+                param("pwpr", "asd")
             }
         }
         finishBuildTrigger {
